225ee1cfd7abb4c1596bd9251ebdf4d49f06967b
matcharr <you@example.com>
2020-08-11 16:48:39 +0200

    Done

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,22 +1,57 @@
 class GossipsController < ApplicationController
+  before_action :authenticate_user, only: [:new, :create, :show, :edit, :destroy]
+
   def new
+    @gossip = Gossip.new
   end
 
   def create
-    @gossip = Gossip.new(title: params["gossip_title"], content: params["gossip_content"])
-    city = City.new
-    user = User.new
-    user.city = city
-    @gossip.user = user
+    @gossip = Gossip.new(gossips_params)
+    @gossip.user = current_user
     if @gossip.save
-      redirect_to '/gossips/'
+      redirect_to gossips_path
     else
       render 'new'
     end
-
-  
   end
 
   def index
+    @gossips = Gossip.all
+  end
+
+  def show
+    @gossip = gossips_finder
+  end
+
+  def edit
+    @gossip = gossips_finder
+  end
+
+  def update
+    @gossip = gossips_finder
+    @gossip.update(gossips_params)
+    redirect_to gossips_path
+  end
+
+  def destroy
+    @gossip = gossips_finder
+    @gossip.delete
+    redirect_to gossips_path
+  end
+
+  private
+
+  def gossips_params
+    params.require(:gossip).permit(:title, :content)
+  end
+
+  def gossips_finder
+    Gossip.find(params[:id])
+  end
+
+  def authenticate_user
+    if !current_user
+      redirect_to new_session_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'user/:id', to: 'user#show', as: 'user'
-  get 'potin/:id', to: 'potin#show', as: 'potin'
   get 'welcome/:id', to: 'welcome#users'
   get '/contact', to: 'contact#contact'
   get '/team', to: 'equipe#team'
   get '/', to: 'accueil#home', as: 'home'
   resources :gossips
+  resources :users, only: [:show, :new, :create]
+  resources :city, only: [:show]
+  resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
